b51a7b55eaec089df341b38771967fa41fae917d
Iascker <mezackisilver@gmail.com>
2023-08-14 09:08:27 -0300

    sdm845-common: Implement dtbhtool from lineageOS hardware/samsung

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -85,7 +85,8 @@ TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld"
 TARGET_KERNEL_ADDITIONAL_FLAGS := \ DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
 
 # Mkbootimg
-BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := $(COMMON_PATH)/configs/hardware/samsung/mkbootimg.mk
 
 # Partitions
 BOARD_ROOT_EXTRA_FOLDERS := efs

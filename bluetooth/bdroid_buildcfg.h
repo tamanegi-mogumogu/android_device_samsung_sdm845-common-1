b2cd845559f3f80cca6193068cabfe98aa4cd087
tamanegi-mogumogu <110730536+tamanegi-mogumogu@users.noreply.github.com>
2023-09-19 18:11:42 +0900

    sdm845-common: Import common bdroid_buildcfg

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,6 +1,10 @@
 /*
+ *  Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ *  Not a Contribution, Apache license notifications and license are retained
+ *  for attribution purposes only.
+ *
  * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2014 The CyanogenMod Project <http://www.cyanogenmod.org>
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,36 +22,10 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#pragma push_macro("PROPERTY_VALUE_MAX")
-
-#include <cutils/properties.h>
-#include <string.h>
-
-static inline const char* BtmGetDefaultName()
-{
-    char product_device[PROPERTY_VALUE_MAX];
-    property_get("ro.product.device", product_device, "");
-    
-    if (strstr(product_device, "starqltechn"))
-        return "Galaxy S9";
-    if (strstr(product_device, "star2qltechn"))
-        return "Galaxy S9+";
-    if (strstr(product_device, "crownqltechn"))
-        return "Galaxy Note 9";
-    
-    // Fallback to Default
-    return "Samsung Galaxy";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
-
-#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
-#define BTIF_HF_WBS_PREFERRED FALSE /* Don't prefer WBS    */
-
-#define BLE_VND_INCLUDED TRUE
-
-#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
-
-#pragma pop_macro("PROPERTY_VALUE_MAX")
-
+// Disables read remote device feature
+#define MAX_ACL_CONNECTIONS   16
+#define MAX_L2CAP_CHANNELS    16
+#define BLE_VND_INCLUDED   TRUE
+/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
+#define AVDT_NUM_SEPS 12
 #endif

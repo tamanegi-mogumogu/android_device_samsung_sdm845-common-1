8522a843674203fe5449877d90ac7b2dd3a6e5d7
EnderMan845 <endearvouruser@gmail.com>
2023-10-30 03:46:29 +0100

    sdm845-common: drop system_ext configs releasetools: remove dtbo ppfiles: definied libsdprpc

--- releasetools.py
+++ releasetools.py
@@ -1,6 +1,6 @@
-# Copyright (C) 2009 The Android Open Source Project
-# Copyright (c) 2011, The Linux Foundation. All rights reserved.
-# Copyright (C) 2017-2022 The LineageOS Project
+#!/bin/env python3
+#
+# Copyright (C) 2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,33 +17,15 @@
 import common
 import re
 
-def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
-  return
-
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
   return
 
-def FullOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.input_zip)
-  return
-
-def IncrementalOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.target_zip)
-  return
-
-def AddImage(info, basename, dest):
-  path = "IMAGES/" + basename
-  if path not in info.input_zip.namelist():
-    return
-
-  data = info.input_zip.read(path)
+def AddImage(info, dir, basename, dest):
+  data = info.input_zip.read(dir + "/" + basename)
   common.ZipWriteStr(info.output_zip, basename, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
-def OTA_InstallEnd(info):
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
-  return
+def PrintInfo(info, dest):
+  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))

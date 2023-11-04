bd3f9753105ece3e822468b664ed31def523ef08
Tqmane845 <110730536+tamanegi-mogumogu@users.noreply.github.com>
2023-11-04 20:54:13 +0900

    initial

--- common.mk
+++ common.mk
@@ -226,7 +226,8 @@ PRODUCT_PACKAGES += \
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
-    libprotobuf-cpp-full
+    libprotobuf-cpp-full \
+    libgui_vendor
 
 # Dolby
 PRODUCT_COPY_FILES += \
